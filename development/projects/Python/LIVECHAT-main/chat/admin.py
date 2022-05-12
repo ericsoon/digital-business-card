835c192882bf925afbd84882cad77f5264e001b5
ericsoon <34089934+ericsoon@users.noreply.github.com>
2022-05-12 13:37:26 -0700

    my first react project

--- development/projects/Python/LIVECHAT-main/chat/admin.py
+++
@@ -1,8 +0,0 @@
-from email import message
-from django.contrib import admin
-from .models import Room, Message
-
-# Register your models here.
-
-admin.site.register(Room)
-admin.site.register(Message)
--- development/projects/Python/LIVECHAT-main/chat/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name = 'home'),
-    path('logIn/', views.loginpage, name = 'login'),
-    path('signUp/', views.register, name = 'register'),
-    path('inbox/', views.inbox, name = 'inbox')
-]
